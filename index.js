791739713952d6e1b1b74bf61689fa089073ef07
meffersino <michaelpjeffrey@hotmail.co.uk>
2017-01-25 16:22:11 +0000

    Added Servo initialisation
    Basic initialisation to set to random values, values to be changed when
    servos arrive and roboto built

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
 var five = require('johnny-five'),
+	servoA, servoB, servoC,
 	board = new five.Board();
+
+board.on("ready", function() {
+	servoA = new five.Servo({
+		pin: 10,
+		range: [1,180],
+		startAt: 60
+	});
+	servoB = new five.Servo({
+		pin: 11,
+		range: [1,180],
+		startAt: 60
+	});
+	ServoC = new five.Servo({
+		pin: 12,
+		range: [1,180],
+		startAt: 60
+	});
+});
 
 console.log("Waiting for device to connect...");
