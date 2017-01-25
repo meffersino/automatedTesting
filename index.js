99d132418232e89ebcfd5ce16701aaf5c680d447
meffersino <michaelpjeffrey@hotmail.co.uk>
2017-01-25 16:34:24 +0000

    Added 'tap' function
    Should work, adds set value to the current position of the servos, waits
    and then returns them to their previous value

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var five = require('johnny-five'),
 	servoA, servoB, servoC,
+	positionA, positionB, positionC,
 	board = new five.Board();
 
 board.on("ready", function() {
@@ -19,5 +20,15 @@ board.on("ready", function() {
 		startAt: 60
 	});
 });
+
+function tap() {
+	servoA.to(positionA + 10);
+	servoB.to(positionB + 10);
+	servoC.to(positionC + 10);
+	setTimeout(myFunction(), 100);
+	servoA.to(positionA);
+	servoB.to(positionB);
+	servoC.to(positionC);
+}
 
 console.log("Waiting for device to connect...");
